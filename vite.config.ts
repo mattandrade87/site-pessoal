d8ff48ee5a4b5f7408749ac41e5933b677706d97
Mateus Andrade <mateus.vitor.andrade@gmail.com>
2025-06-02 17:46:18 -0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/site-pessoal/',
   plugins: [react(), tailwindcss()],
-})
+  base: "/site-pessoal/",
+});
